dbca3893054e8952374c54d811bf8e10c644c1d9
coolrunner1 <waurttttt@gmail.com>
2025-05-07 11:10:09 +0300

    Renamed CheckAdmin middleware to ProtectAdmin

--- example-app/bootstrap/app.php
+++ example-app/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: \App\Http\Middleware\LogVisitor::class);
-        $middleware->web(append: \App\Http\Middleware\CheckAdmin::class);
+        $middleware->web(append: \App\Http\Middleware\ProtectAdmin::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
